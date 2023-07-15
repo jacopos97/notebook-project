d4efd135932b459da279e65973c5fe05aeac3686
jacopo <jacoposabatino@gmail.com>
2023-07-15 16:17:30 +0200

    Finiti i test per l'interfaccia al database MySQL.
    https://github.com/jacopos97/notebook-project/issues/23

--- com.examples.notebook/src/it/java/com/examples/notebook/repository/sql/NotesMySqlRepositoryIT.java
+++ com.examples.notebook/src/it/java/com/examples/notebook/repository/sql/NotesMySqlRepositoryIT.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 import org.junit.After;
 import org.junit.Before;
@@ -21,35 +25,43 @@ import com.mongodb.ServerAddress;
 
 public class NotesMySqlRepositoryIT {
 	
+	private Connection connection;
 	private NotesMySqlRepository notesMySqlRepository;
 	
+	private static final Logger LOGGER = LogManager.getLogger(NotesMySqlRepositoryIT.class);
+	
 	@Before
-	public void setUp() throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.jdbc.Driver");
-		var connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","user","password");
-		notesMySqlRepository = new NotesMySqlRepository(connection);
-		/*client = new MongoClient(new ServerAddress("localhost", mongoPort));
-		var database = client.getDatabase(NOTEBOOK_DB_NAME);
-		database.drop();
-		notesCollection = database.getCollection(NOTE_COLLECTION_NAME);*/
+	public void setUp(){
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/notebook","user","password");
+			notesMySqlRepository = new NotesMySqlRepository(connection);
+			var truncateStatement = connection.createStatement();
+			truncateStatement.executeUpdate("truncate notebook.Notes");
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
 	}
 	
 	@After
 	public void tearDown() {
-		//client.close();
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
 	}
 
 	
-	/*@Test
+	@Test
 	public void testFindAllWhenDatabaseIsEmpty() {
-		assertThat(notesMongoRepository.findAll()).isEmpty();;
+		assertThat(notesMySqlRepository.findAll()).isEmpty();;
 	}
 	
 	@Test
 	public void testFindAllWhenDatabaseIsNotEmpty() {
 		addTestNoteToDatabase("2000/01/01", "Title1", "Body1");
 		addTestNoteToDatabase("2000/01/02", "Title2", "Body2");
-		assertThat(notesMongoRepository.findAll())
+		assertThat(notesMySqlRepository.findAll())
 			.containsExactly(
 				new Note("2000/01/01", "Title1", "Body1"),
 				new Note("2000/01/02", "Title2", "Body2"));
@@ -57,7 +69,7 @@ public class NotesMySqlRepositoryIT {
 	
 	@Test
 	public void testFindByIdNotFound() {
-		assertThat(notesMongoRepository.findById("2000/01/01-Title"))
+		assertThat(notesMySqlRepository.findById("2000/01/01-Title"))
 			.isNull();
 	}
 	
@@ -65,14 +77,14 @@ public class NotesMySqlRepositoryIT {
 	public void testFindByIdFound() {
 		addTestNoteToDatabase("2000/01/01", "Title1", "Body1");
 		addTestNoteToDatabase("2000/01/02", "Title2", "Body2");
-		assertThat(notesMongoRepository.findById("2000/01/02-Title2"))
+		assertThat(notesMySqlRepository.findById("2000/01/02-Title2"))
 			.isEqualTo(new Note("2000/01/02", "Title2", "Body2"));
 	}
 	
 	@Test
 	public void testSave() {
 		var note = new Note("2000/01/01", "Title", "Body");
-		notesMongoRepository.save(note);
+		notesMySqlRepository.save(note);
 		assertThat(readAllNotesFromDatabase())
 			.containsExactly(note);
 	}
@@ -80,7 +92,7 @@ public class NotesMySqlRepositoryIT {
 	@Test
 	public void testDelete() {
 		addTestNoteToDatabase("2000/01/01", "Title", "Body");
-		notesMongoRepository.delete("2000/01/01-Title");
+		notesMySqlRepository.delete("2000/01/01-Title");
 		assertThat(readAllNotesFromDatabase()).isEmpty();
 	}
 	
@@ -88,24 +100,34 @@ public class NotesMySqlRepositoryIT {
 	public void testModify() {
 		addTestNoteToDatabase("2000/01/01", "OldTitle", "OldBody");
 		var noteModified = new Note("2001/02/02", "NewTitle", "NewBody");
-		notesMongoRepository.modify("2000/01/01-OldTitle", noteModified);
+		notesMySqlRepository.modify("2000/01/01-OldTitle", noteModified);
 		assertThat(readAllNotesFromDatabase()).containsExactly(noteModified);
 	}
 
 	private List<Note> readAllNotesFromDatabase() {
-		return StreamSupport
-				.stream(notesCollection.find().spliterator(), false)
-				.map(d -> new Note(""+d.get("date"), ""+d.get("title"), ""+d.get("body")))
-				.toList();
+		var noteList = new ArrayList<Note>();
+		try {
+			var tableRow = connection.createStatement().executeQuery("select * from notebook.Notes");
+			while (tableRow.next())
+				noteList.add(new Note(tableRow.getString("NoteDate"), tableRow.getString("Title"), tableRow.getString("Body")));
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
+		return noteList;
 	}
 
 	private void addTestNoteToDatabase(String date, String title, String body) {
-		notesCollection.insertOne(
-				new Document()
-				.append("date", date)
-				.append("title", title)
-				.append("body", body)
-				.append("id", date + "-" + title));
-	}*/
+		String query = " insert into notebook.Notes values (?, ?, ?, ?)";
+		try {
+			var preparedStmt = connection.prepareStatement(query);
+			preparedStmt.setString(1, date);
+			preparedStmt.setString(2, title);
+			preparedStmt.setString(3, body);
+			preparedStmt.setString(4, date + "-" + title);
+			preparedStmt.execute();
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
+	}
 
 }
--- com.examples.notebook/src/main/java/com/examples/notebook/repository/sql/NotesMySqlRepository.java
+++ com.examples.notebook/src/main/java/com/examples/notebook/repository/sql/NotesMySqlRepository.java
@@ -4,77 +4,130 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bson.Document;
+
+import com.examples.notebook.app.NotebookApp;
 import com.examples.notebook.model.Note;
 import com.examples.notebook.repository.NotesRepository;
 
 public class NotesMySqlRepository implements NotesRepository {
+	
+	private Connection connection;
+	
+	private static final Logger LOGGER = LogManager.getLogger(NotesMySqlRepository.class);
 
-	public static void main(String args[]) {
+	/*public static void main(String args[]) {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Class.forName("com.mysql.cj.jdbc.Driver");
 			var con = DriverManager.getConnection("jdbc:mysql://localhost:3306/notebook", "user", "password");
 			var stmt = con.createStatement();
 			var rs = stmt.executeQuery("select * from Notes");
-			
-			String query = " insert into notebook.Notes"+ " values (?, ?, ?, ?)";
+
+			String query = " insert into notebook.Notes" + " values (?, ?, ?, ?)";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			preparedStmt.setString(1, "2000/10/10");
 			preparedStmt.setString(2, "Title");
 			preparedStmt.setString(3, "Body");
 			preparedStmt.setString(4, "2000/10/10-Title");
 			preparedStmt.execute();
-			
-			while (rs.next())
-				System.out.println(rs.getString(1) + "  " + rs.getString(2) + "  " + rs.getString(3)+ "  " + rs.getString(4));
-			/*var metadata = con.getMetaData();
-            ResultSet resultSet = metadata.getTables(null, null, null, null);
-            while (resultSet.next()) {
-                String tableName = resultSet.getString("TABLE_NAME");
-                System.out.println(tableName);*/
-            //resultSet.close();
-			con.close();
-		}catch(
 
-	Exception e)
-	{
-		System.out.println(e);
-	}
-	}
+			while (rs.next())
+				System.out.println(
+						rs.getString(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  " + rs.getString(4));
+			
+			  var metadata = con.getMetaData(); ResultSet resultSet =
+			  metadata.getTables(null, null, null, null); while (resultSet.next()) { String
+			  tableName = resultSet.getString("TABLE_NAME"); System.out.println(tableName);
+			 
+			// resultSet.close();
+			con.close();
+		} catch (
+		Exception e) {
+			System.out.println(e);
+		}
+	}*/
 
 	public NotesMySqlRepository(Connection connection) {
-		// connection
+		this.connection = connection;
 	}
 
 	@Override
 	public List<Note> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		var noteList = new ArrayList<Note>();
+		try {
+			var tableRow = connection.createStatement().executeQuery("select * from notebook.Notes");
+			while (tableRow.next())
+				noteList.add(fromRowElementToNote(tableRow));
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
+		return noteList;
 	}
 
 	@Override
 	public Note findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Note note = null;
+		try {
+			var tableRow = connection.createStatement().executeQuery("select * from notebook.Notes where Id='" + id + "'");
+			if (tableRow.next())
+				note = fromRowElementToNote(tableRow);
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
+		return note;
 	}
 
 	@Override
 	public void save(Note noteToAdd) {
-		// TODO Auto-generated method stub
+		String query = " insert into notebook.Notes values (?, ?, ?, ?)";
+		try {
+			var preparedStmt = connection.prepareStatement(query);
+			preparedStmt.setString(1, noteToAdd.getDate());
+			preparedStmt.setString(2, noteToAdd.getTitle());
+			preparedStmt.setString(3, noteToAdd.getBody());
+			preparedStmt.setString(4, noteToAdd.getId());
+			preparedStmt.execute();
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
 
 	}
 
 	@Override
 	public void delete(String idNoteToDelete) {
-		// TODO Auto-generated method stub
+		try {
+			connection.createStatement().executeUpdate("delete from notebook.Notes where Id='" + idNoteToDelete + "'");
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
 
 	}
 
 	@Override
-	public void modify(String idNoteToModify, Note noteToModify) {
-		// TODO Auto-generated method stub
-
+	public void modify(String idNoteToModify, Note noteModified) {
+		try {
+			connection.createStatement().executeUpdate(
+					"update notebook.Notes set " +
+					"NoteDate='" + noteModified.getDate() +
+					"', Title='" + noteModified.getTitle() +
+					"', Body='" + noteModified.getBody() +
+					"', Id='" + noteModified.getId() +
+					"' where Id='" + idNoteToModify + "'");
+		} catch (SQLException e) {
+			LOGGER.error("SQLException", e);
+		}
+	}
+	
+	private Note fromRowElementToNote(ResultSet row) throws SQLException {
+		return new Note(row.getString("NoteDate"), row.getString("Title"), row.getString("Body"));
 	}
 
 }
