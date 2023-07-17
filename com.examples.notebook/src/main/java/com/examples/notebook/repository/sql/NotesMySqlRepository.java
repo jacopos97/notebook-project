9ca2054b481f30f2754d961f6e711a05c61dcb7e
jacopo <jacoposabatino@gmail.com>
2023-07-17 19:23:47 +0200

    Modicfiche all'interfaccia del database MySQL.
    Sistemate le classi relative al database MySQL cosicché il nome del
    database e della tabella sia passato da fuori. Ciò ha richiesto la
    modifica anche del POM, in quanto è stata cambiata l'immagine del
    container MySQL.
    
    https://github.com/jacopos97/notebook-project/issues/25

--- com.examples.notebook/src/main/java/com/examples/notebook/repository/sql/NotesMySqlRepository.java
+++ com.examples.notebook/src/main/java/com/examples/notebook/repository/sql/NotesMySqlRepository.java
@@ -13,26 +13,24 @@ import com.examples.notebook.model.Note;
 import com.examples.notebook.repository.NotesRepository;
 
 public class NotesMySqlRepository implements NotesRepository {
-
-	private static final String SELECT_ALL_NOTES = "select * from notebook.Notes";
-	private static final String SELECT_NOTE_BY_ID = "select * from notebook.Notes where Id=?";
-	private static final String SAVE_NOTE = "insert into notebook.Notes values (?, ?, ?, ?)";
-	private static final String DELETE_NOTE = "delete from notebook.Notes where Id=?";
-	private static final String MODIFY_NOTE = "update notebook.Notes set NoteDate=?, Title=?, Body=?, Id=? where Id=?";
+	
 	private static final String SQL_EXCEPTION_MESSAGE = "SQLException";
 
 	private Connection connection;
+	private String tableName;
 
 	private static final Logger LOGGER = LogManager.getLogger(NotesMySqlRepository.class);
 
-	public NotesMySqlRepository(Connection connection) {
+	public NotesMySqlRepository(Connection connection, String tableName) {
 		this.connection = connection;
+		this.tableName = tableName;
 	}
 
 	@Override
 	public List<Note> findAll(){
 		var noteList = new ArrayList<Note>();
-		try (var preparedStatement = connection.prepareStatement(SELECT_ALL_NOTES)) {
+		var query = "select * from " + tableName;
+		try (var preparedStatement = connection.prepareStatement(query)) {
 			var tableRow = preparedStatement.executeQuery();
 			while (tableRow.next())
 				noteList.add(fromRowElementToNote(tableRow));
@@ -45,7 +43,8 @@ public class NotesMySqlRepository implements NotesRepository {
 	@Override
 	public Note findById(String id) {
 		Note note = null;
-		try (var preparedStatement = connection.prepareStatement(SELECT_NOTE_BY_ID)) {
+		var query = "select * from " + tableName + " where Id=?";
+		try (var preparedStatement = connection.prepareStatement(query)) {
 			preparedStatement.setString(1, id);
 			var tableRow = preparedStatement.executeQuery();
 			if (tableRow.next())
@@ -58,7 +57,8 @@ public class NotesMySqlRepository implements NotesRepository {
 
 	@Override
 	public void save(Note noteToAdd) {
-		try (var preparedStatement = connection.prepareStatement(SAVE_NOTE)) {
+		var query = "insert into " + tableName + " values (?, ?, ?, ?)";
+		try (var preparedStatement = connection.prepareStatement(query)) {
 			preparedStatement.setString(1, noteToAdd.getDate());
 			preparedStatement.setString(2, noteToAdd.getTitle());
 			preparedStatement.setString(3, noteToAdd.getBody());
@@ -71,7 +71,8 @@ public class NotesMySqlRepository implements NotesRepository {
 
 	@Override
 	public void delete(String idNoteToDelete) {
-		try (var preparedStatement = connection.prepareStatement(DELETE_NOTE)) {
+		var query = "delete from " + tableName + " where Id=?";
+		try (var preparedStatement = connection.prepareStatement(query)) {
 			preparedStatement.setString(1, idNoteToDelete);
 			preparedStatement.executeUpdate();
 		} catch (SQLException e) {
@@ -82,7 +83,8 @@ public class NotesMySqlRepository implements NotesRepository {
 
 	@Override
 	public void modify(String idNoteToModify, Note noteModified) {
-		try (var preparedStatement = connection.prepareStatement(MODIFY_NOTE)) {
+		var query = "update " + tableName + " set NoteDate=?, Title=?, Body=?, Id=? where Id=?";
+		try (var preparedStatement = connection.prepareStatement(query)) {
 			preparedStatement.setString(1, noteModified.getDate());
 			preparedStatement.setString(2, noteModified.getTitle());
 			preparedStatement.setString(3, noteModified.getBody());
