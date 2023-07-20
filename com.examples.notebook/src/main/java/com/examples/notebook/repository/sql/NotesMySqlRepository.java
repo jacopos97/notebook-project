05d632bf288650541c26cf293d74235909a43d45
jacopo <jacoposabatino@gmail.com>
2023-07-20 18:06:46 +0200

    Modifica gestione eccezione SQL.
    Modificata NotesMySqlRepository per gestire il lancio della eccezione
    SQL in un'unica funzione.
    
    https://github.com/jacopos97/notebook-project/issues/27

--- com.examples.notebook/src/main/java/com/examples/notebook/repository/sql/NotesMySqlRepository.java
+++ com.examples.notebook/src/main/java/com/examples/notebook/repository/sql/NotesMySqlRepository.java
@@ -1,6 +1,7 @@
 package com.examples.notebook.repository.sql;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -30,74 +31,77 @@ public class NotesMySqlRepository implements NotesRepository {
 	public List<Note> findAll(){
 		var noteList = new ArrayList<Note>();
 		var query = "select * from " + tableName;
-		try (var preparedStatement = connectionDatabase.prepareStatement(query)) {
+		manageSqlConnection(preparedStatement -> {
 			var tableRow = preparedStatement.executeQuery();
 			while (tableRow.next())
 				noteList.add(fromRowElementToNote(tableRow));
-		} catch (SQLException e) {
-			LOGGER.error(SQL_EXCEPTION_MESSAGE, e);
-		} 
+		}, query);
 		return noteList;
 	}
 
 	@Override
 	public Note findById(String id) {
-		Note note = null;
+		var arrayLength = 1;
+		var noteWrapper = new Note[arrayLength];
 		var query = "select * from " + tableName + " where Id=?";
-		try (var preparedStatement = connectionDatabase.prepareStatement(query)) {
+		manageSqlConnection(preparedStatement -> {
 			preparedStatement.setString(1, id);
 			var tableRow = preparedStatement.executeQuery();
 			if (tableRow.next())
-				note = fromRowElementToNote(tableRow);
-		} catch (SQLException e) {
-			LOGGER.error(SQL_EXCEPTION_MESSAGE, e);
-		}
-		return note;
+				noteWrapper[arrayLength - 1] = fromRowElementToNote(tableRow);
+		}, query);
+		return noteWrapper[arrayLength - 1];
 	}
 
 	@Override
 	public void save(Note noteToAdd) {
 		var query = "insert into " + tableName + " values (?, ?, ?, ?)";
-		try (var preparedStatement = connectionDatabase.prepareStatement(query)) {
+		manageSqlConnection(preparedStatement -> {
 			preparedStatement.setString(1, noteToAdd.getDate());
 			preparedStatement.setString(2, noteToAdd.getTitle());
 			preparedStatement.setString(3, noteToAdd.getBody());
 			preparedStatement.setString(4, noteToAdd.getId());
 			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			LOGGER.error(SQL_EXCEPTION_MESSAGE, e);
-		}
+		}, query);
 	}
 
 	@Override
 	public void delete(String idNoteToDelete) {
 		var query = "delete from " + tableName + " where Id=?";
-		try (var preparedStatement = connectionDatabase.prepareStatement(query)) {
+		manageSqlConnection(preparedStatement -> {
 			preparedStatement.setString(1, idNoteToDelete);
 			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			LOGGER.error(SQL_EXCEPTION_MESSAGE, e);
-		}
-
+		}, query);
 	}
 
 	@Override
 	public void modify(String idNoteToModify, Note noteModified) {
 		var query = "update " + tableName + " set NoteDate=?, Title=?, Body=?, Id=? where Id=?";
-		try (var preparedStatement = connectionDatabase.prepareStatement(query)) {
+		manageSqlConnection(preparedStatement -> {
 			preparedStatement.setString(1, noteModified.getDate());
 			preparedStatement.setString(2, noteModified.getTitle());
 			preparedStatement.setString(3, noteModified.getBody());
 			preparedStatement.setString(4, noteModified.getId());
 			preparedStatement.setString(5, idNoteToModify);
 			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			LOGGER.error(SQL_EXCEPTION_MESSAGE, e);
-		}
+		}, query);
 	}
 
 	private Note fromRowElementToNote(ResultSet row) throws SQLException {
 		return new Note(row.getString("NoteDate"), row.getString("Title"), row.getString("Body"));
+	}
+	
+	private void manageSqlConnection(ConnectionAction action, String query) {
+		try (PreparedStatement preparedStatement = connectionDatabase.prepareStatement(query)){
+			action.execute(preparedStatement);
+		} catch (SQLException e) {
+			LOGGER.error(SQL_EXCEPTION_MESSAGE, e);
+		}
+	}
+	
+	@FunctionalInterface
+	interface ConnectionAction{
+			void execute(PreparedStatement preparedStatement) throws SQLException;
 	}
 
 }
